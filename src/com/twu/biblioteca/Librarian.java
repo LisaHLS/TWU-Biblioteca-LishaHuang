503463159adc948dc3a17342c2b60ce9ad02a02e
LisaHuang <1449700087@qq.com>
2018-04-27 16:21:12 +0800

    add test case should_return_thank_you_enjoy_the_book_when_check_out_success in LibrarianTest.java

--- src/com/twu/biblioteca/Librarian.java
+++ src/com/twu/biblioteca/Librarian.java
@@ -2,4 +2,11 @@ package com.twu.biblioteca;
 
 public class Librarian {
 
+    public String checkOut(Book book){
+        return null;
+    }
+
+    public String returnBook(Book book){
+        return null;
+    }
 }
--- test/com/twu/biblioteca/LibrarianTest.java
+++ test/com/twu/biblioteca/LibrarianTest.java
@@ -1,5 +1,23 @@
 package com.twu.biblioteca;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class LibrarianTest {
 
+    private Librarian librarian;
+    private Book book;
+
+    @Before
+    public void setUp() {
+        librarian = new Librarian();
+        book = new Book("Head First Java","Kent Belt",2003);
+    }
+
+    @Test
+    public void should_return_thank_you_enjoy_the_book_when_check_out_success() {
+        assertEquals(librarian.checkOut(book), "Thank you! Enjoy the book");
+    }
 }
