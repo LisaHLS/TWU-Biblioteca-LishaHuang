b4a0268628e3a064d16ba756a5fa55571e593712
LisaHuang <1449700087@qq.com>
2018-04-27 13:55:47 +0800

    create BookTest.java and add test case should_book_have_name_author_publishedYear()

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void setUp() {
+        book = new Book("Head First Java","Kent Belt",2003);
+    }
+
+    @Test
+    public void should_book_have_name_author_publishedYear() {
+
+        assertEquals(book.toString(), "Head First Java     Kent Belt     2003\n");
+    }
+}
