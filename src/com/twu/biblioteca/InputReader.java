9ca30b375cb90a3cec7e3f790f46fa2a352b273c
LisaHuang <1449700087@qq.com>
2018-04-28 09:58:31 +0800

    create InputReader.java

---
+++ src/com/twu/biblioteca/InputReader.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class InputReader {
+
+    public String readOption() {
+        return null;
+    }
+
+    public String readBook() {
+        return null;
+    }
+
+}
