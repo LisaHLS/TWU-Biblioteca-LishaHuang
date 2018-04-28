57cff631afeef32805fc40a2187e2c22f745d048
LisaHuang <1449700087@qq.com>
2018-04-28 11:25:28 +0800

    create Menu.java

---
+++ src/com/twu/biblioteca/Menu.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+public class Menu {
+
+    public void printWelcomeMsg() {
+
+    }
+
+    public void printAllOptions() {
+    }
+
+    public boolean processingBusinessAccordingToOption () {
+        return true;
+    }
+
+    public void printBooksList() {
+
+    }
+
+    public void printGoodByeMsg() {
+
+    }
+
+    public void checkOut() {
+
+    }
+
+    public void returnBook() {
+    }
+
+}
