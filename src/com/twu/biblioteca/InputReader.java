ca274fb28f39128039894c4984cfd1b41a34577d
LisaHuang <1449700087@qq.com>
2018-04-28 10:33:21 +0800

    implement method readOption() in InputReader.java and pass test cases in InputReaderTest.java

--- src/com/twu/biblioteca/InputReader.java
+++ src/com/twu/biblioteca/InputReader.java
@@ -1,9 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class InputReader {
 
+    private Scanner scanner;
+
+    public InputReader() {
+        scanner = new Scanner(System.in);
+    }
+
     public String readOption() {
-        return null;
+        String input = scanner.next().trim();
+        return Pattern.compile("[1-4]").matcher(input).matches() ? input : "Select a valid option!";
     }
 
     public String readBook() {
