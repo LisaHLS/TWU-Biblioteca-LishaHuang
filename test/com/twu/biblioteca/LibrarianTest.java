3786997638d70441e244edb9c3621417e9563edc
LisaHuang <1449700087@qq.com>
2018-04-30 15:30:46 +0800

    add test cases for check out movie in LibrarianTest.java

--- test/com/twu/biblioteca/LibrarianTest.java
+++ test/com/twu/biblioteca/LibrarianTest.java
@@ -11,6 +11,7 @@ public class LibrarianTest {
     private Librarian librarian;
     private Book book;
     private UserAccounts userAccounts;
+    private Movie movie;
 
     @Before
     public void setUp() {
@@ -18,6 +19,7 @@ public class LibrarianTest {
         book = new Book("Head First Java","Kent Belt",2003);
         userAccounts = new UserAccounts();
         userAccounts.login("110-1234", "123456");
+        movie = new Movie("The Great Buddha+", 2017, "Xinyao Huang", "9");
 
     }
 
@@ -29,7 +31,7 @@ public class LibrarianTest {
     }
 
     @Test
-    public void should_return_false_when_check_out_fail() {
+    public void should_return_false_when_check_out_book_fail() {
         librarian.checkOut(book);
         assertFalse(librarian.checkOut(book));
     }
@@ -63,5 +65,15 @@ public class LibrarianTest {
         assertFalse(librarian.returnBook(book));
         assertTrue(!librarian.getBookCheckOutRecord().containsKey(book)
             && !librarian.getBookCheckOutRecord().containsValue(UserAccounts.currentLoginUser));
+    }
+
+    @Test
+    public void should_return_true_when_check_out_movie_succeed() {
+        assertTrue(librarian.checkOutMovie(movie));
+    }
+
+    @Test
+    public void should_return_false_when_check_out_movie_fail() {
+        assertFalse(librarian.checkOutMovie(movie));
     }
 }
