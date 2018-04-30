a221861715a39004dbcdc2dc76166a82e28df570
LisaHuang <1449700087@qq.com>
2018-04-30 11:52:58 +0800

    create entity Movie.java

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,51 @@
+package com.twu.biblioteca;
+
+public class Movie {
+
+    private String name;
+
+    private int year;
+
+    private String director;
+
+    private int movieRating;
+
+    public Movie(String name, int year, String director, int movieRating) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.movieRating = movieRating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(int movieRating) {
+        this.movieRating = movieRating;
+    }
+}
