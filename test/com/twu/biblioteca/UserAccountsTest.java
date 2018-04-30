d2e3f0d8c9f2fe309e15f1ac394e897c298c2b5f
LisaHuang <1449700087@qq.com>
2018-04-30 10:58:35 +0800

    add test cases in UserAccountsTest.java for user login

--- test/com/twu/biblioteca/UserAccountsTest.java
+++ test/com/twu/biblioteca/UserAccountsTest.java
@@ -1,5 +1,35 @@
 package com.twu.biblioteca;
 
-public class UserAccountsTest {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserAccountsTest {
+    private UserAccounts userAccounts;
+
+    @Before
+    public void setUp() {
+        userAccounts = new UserAccounts();
+    }
+
+    @Test
+    public void should_return_true_when_user_login_succeed() {
+        assertTrue(userAccounts.login("110-1234", "123456"));
+    }
+
+    @Test
+    public void should_return_false_when_libraryNumber_is_wrong() {
+        assertTrue(userAccounts.login("1101234", "123456"));
+    }
+
+    @Test
+    public void should_return_false_when_password_is_wrong() {
+        assertTrue(userAccounts.login("110-1234", "1111111"));
+    }
+
+    @Test
+    public void should_return_false_when_user_not_exist() {
+        assertTrue(userAccounts.login("199-9876", "123456"));
+    }
 }
