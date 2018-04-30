4002ae4395c05f71adbcea54214e4b06066072bc
LisaHuang <1449700087@qq.com>
2018-04-30 11:44:32 +0800

    add should_return_user_name_email_phone_when_user_login test case in UserAccountsTest.java

--- test/com/twu/biblioteca/UserAccountsTest.java
+++ test/com/twu/biblioteca/UserAccountsTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -32,5 +33,11 @@ public class UserAccountsTest {
     @Test
     public void should_return_false_when_user_not_exist() {
         assertFalse(userAccounts.login("199-9876", "123456"));
+    }
+
+    @Test
+    public void should_return_user_name_email_phone_when_user_login() {
+        userAccounts.login("110-1234", "123456");
+        assertEquals(userAccounts.showUserInformation(), " name: Jack, emailAddress: 123@163.com, phoneNumber: 15123476587");
     }
 }
