13f20b3ebdcef0db37af98d036f69f791bd926fc
LisaHuang <1449700087@qq.com>
2018-04-30 18:35:02 +0800

    add and modify test cases

--- test/com/twu/biblioteca/InputReaderTest.java
+++ test/com/twu/biblioteca/InputReaderTest.java
@@ -26,87 +26,77 @@ public class InputReaderTest {
     }
 
     @Test
-    public void should_return_input_option_itself_1_when_input_is_digit_and_in_options () throws NoSuchFieldException, IllegalAccessException {
+    public void should_return_input_itself_when_user_option_is_digit_and_in_options_else_return_prompt_msg () throws NoSuchFieldException, IllegalAccessException {
         setInputStream("1");
-        assertThat(reader.readOption()).isEqualTo("1");
-    }
-
-    @Test
-    public void should_return_input_option__itself_4_when_input_is_digit_and_in_options () throws NoSuchFieldException, IllegalAccessException {
-        setInputStream("4");
-        assertThat(reader.readOption()).isEqualTo("4");
-    }
-
-    @Test
-    public void should_return_select_a_valid_option_when_input_5_is_not_in_options () throws NoSuchFieldException, IllegalAccessException {
-        setInputStream("5");
-        assertThat(reader.readOption()).isEqualTo("Select a valid option!");
-    }
-
-    @Test
-    public void should_return_select_a_valid_option_when_input_is_not_digit () throws NoSuchFieldException, IllegalAccessException {
+        assertThat(reader.readUserOption()).isEqualTo("1");
+        setInputStream("7");
+        assertThat(reader.readUserOption()).isEqualTo("7");
+        setInputStream("8");
+        assertThat(reader.readUserOption()).isEqualTo("Select a valid option!");
         setInputStream("a");
-        assertThat(reader.readOption()).isEqualTo("Select a valid option!");
+        assertThat(reader.readUserOption()).isEqualTo("Select a valid option!");
     }
 
     @Test
-    public void should_return_book_info_when_input_has_all_book_info_and_order_is_name_author_publishedYear () throws NoSuchFieldException, IllegalAccessException {
+    public void should_return_input_itself_when_input_has_all_book_info_and_order_is_name_author_publishedYear_else_return_prompt_msg () throws NoSuchFieldException, IllegalAccessException {
         setInputStream("<Head First Java>,Kent Belt,2003");
         assertThat(reader.readBook()).isEqualTo("<Head First Java>,Kent Belt,2003");
-    }
-
-    @Test
-    public void should_return_that_book_info_is_invalid_when_input_not_has_name() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("Kent Belt,2003");
         assertThat(reader.readBook()).isEqualTo("That book information is invalid");
-    }
-
-    @Test
-    public void should_return_that_book_info_is_invalid_when_input_not_has_author() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("<Head First Java>,2003");
         assertThat(reader.readBook()).isEqualTo("That book information is invalid");
-    }
-
-    @Test
-    public void should_return_that_book_info_is_invalid_when_input_not_has_publishedYear() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("<Head First Java>,Kent Belt");
         assertThat(reader.readBook()).isEqualTo("That book information is invalid");
-    }
-
-    @Test
-    public void should_return_that_book_info_is_invalid_when_input_order_not_as_name_author_publishedYear() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("<Head First Java>,2003,Kent Belt");
         assertThat(reader.readBook()).isEqualTo("That book information is invalid");
-    }
-
-    @Test
-    public void should_return_that_book_info_is_invalid_when_publishedYear_is_not_digit() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("<Head First Java>,Kent Belt,jjj");
         assertThat(reader.readBook()).isEqualTo("That book information is invalid");
     }
 
     @Test
-    public void should_return_input_itself_when_libraryNumber_and_password_is_valid() throws NoSuchFieldException, IllegalAccessException {
+    public void should_return_input_itself_when_libraryNumber_and_password_is_valid_else_return_prompt_msg() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("110-1234,123456");
         assertThat(reader.readLibraryNumberAndPassword()).isEqualTo("110-1234,123456");
-    }
-
-    @Test
-    public void should_return_prompt_msg_when_libraryNumber_and_password_is_invalid() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("1101234,123456");
         assertThat(reader.readLibraryNumberAndPassword()).isEqualTo("That libraryNumber or password is invalid");
     }
 
     @Test
-    public void should_return_choose_when_input_is_1_or_2() throws NoSuchFieldException, IllegalAccessException {
+    public void should_return_itself_when_choose_is_in_1_to_3_and_else_return_prompt_msg() throws NoSuchFieldException, IllegalAccessException {
         setInputStream("1");
         assertThat(reader.readChooseUserOrLibrarian()).isEqualTo("1");
+        setInputStream("3");
+        assertThat(reader.readChooseUserOrLibrarian()).isEqualTo("3");
+        setInputStream("4");
+        assertThat(reader.readChooseUserOrLibrarian()).isEqualTo("That choose is invalid");
     }
 
     @Test
-    public void should_return_choose_when_input_not_1_and_2() throws NoSuchFieldException, IllegalAccessException {
+    public void should_return_input_itself_when_movie_has_name_year_director_and_order_is_right_else_return_prompt_msg () throws NoSuchFieldException, IllegalAccessException {
+        setInputStream("<The Post>,2017,Steven Spielberg");
+        assertThat(reader.readMovie()).isEqualTo("<The Post>,2017,Steven Spielberg");
+        setInputStream("2017,Steven Spielberg");
+        assertThat(reader.readMovie()).isEqualTo("That movie information is invalid");
+        setInputStream("<The Post>,2017");
+        assertThat(reader.readMovie()).isEqualTo("That movie information is invalid");
+        setInputStream("<The Post>,Steven Spielberg");
+        assertThat(reader.readMovie()).isEqualTo("That movie information is invalid");
+        setInputStream("<The Post>,Steven Spielberg,2017");
+        assertThat(reader.readMovie()).isEqualTo("That movie information is invalid");
+        setInputStream("<The Post>,kkk,Steven Spielberg");
+        assertThat(reader.readMovie()).isEqualTo("That movie information is invalid");
+    }
+
+    @Test
+    public void should_return_input_itself_when_librarian_option_is_digit_and_in_options_else_return_prompt_msg () throws NoSuchFieldException, IllegalAccessException {
+        setInputStream("1");
+        assertThat(reader.readLibrarianOption()).isEqualTo("1");
         setInputStream("3");
-        assertThat(reader.readChooseUserOrLibrarian()).isEqualTo("That choose is invalid");
+        assertThat(reader.readLibrarianOption()).isEqualTo("3");
+        setInputStream("4");
+        assertThat(reader.readLibrarianOption()).isEqualTo("Select a valid option!");
+        setInputStream("a");
+        assertThat(reader.readLibrarianOption()).isEqualTo("Select a valid option!");
     }
 
 }
